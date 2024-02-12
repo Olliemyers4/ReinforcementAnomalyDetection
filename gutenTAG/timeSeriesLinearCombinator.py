b88058982ecf52ef1173ea6309259b72d8e29256
Olliemyers4 <olliemyers4@gmail.com>
2024-02-12 12:33:43 +0000

    Linear Combination of timeseries

--- gutenTAG/timeSeriesLinearCombinator.py
+++ gutenTAG/timeSeriesLinearCombinator.py
@@ -1,5 +1,6 @@
 import os
-
+import pandas as pd
+from matplotlib import pyplot as plt
 
 cwd = os.getcwd()
 topFolderForGenerated = os.path.join(cwd, "generated-timeseries")
@@ -19,6 +20,7 @@ for root, dirs, files in os.walk(topFolderForGenerated):
         if file.endswith(".csv"):
             csvFiles.append(os.path.join(root, file))
 
+
 print("Found the following csv files:")
 for i in range(0,len(csvFiles)):
     print(f"File {i}: {file}")
@@ -27,9 +29,43 @@ print("Which file would you like to use?")
 fileIndex = int(input("File Index:"))
 file = csvFiles[fileIndex]
 
-with open(file, "r") as f:
-    lines = f.readlines()
+df = pd.read_csv(file)
 
-print("The first 5 lines of the file are:")
-for i in range(0,5):
-    print(lines[i])
+timeseries = len(df.columns) - 2 # -2 for timestamp is_anomaly
+#As long as we dont do any weird shuffling we can ignore the timestamp column and anomaly column
+#ignore 0th col and take last col for each col in the timeseries
+fig, axs = plt.subplots(timeseries+1)
+for i in range(1, timeseries+1):
+    axs[i-1].plot(df[df.columns[i]])
+    axs[i-1].set_title(df.columns[i])
+#Add the anomaly
+axs[timeseries].plot(df[df.columns[timeseries+1]])
+axs[timeseries].set_title(df.columns[timeseries+1])
+plt.show(block=False) #we can continue getting UserInput
+
+totalTimeSeries = int(input("How many total time series do you want?"))
+
+newDF = pd.DataFrame()
+newDF.insert(0, "timestamp",df[df.columns[0]],True) #timestamp
+
+for i in range(0,totalTimeSeries):
+    print(f"Timeseries {i}:")
+    print("\n")
+    option = int(input("0 for using an existing time series, 1 for a linear combination:"))
+
+    if option == 0:
+        waveIndex = int(input("Which timeseries would you like to use?"))
+        newDF.insert(len(newDF.columns), f"value-{i}",df[df.columns[waveIndex]],True)
+    else:
+        combined = int(input("How many timeseries would you like to combine?"))
+        combinedSeries = [0] *len(df[df.columns[0]])
+        for j in range(0,combined):
+            waveIndex = int(input("Which timeseries would you like to use?"))
+            multiplier = float(input("What multiplier would you like to use?(float)"))
+            multiplied = df[df.columns[waveIndex]]*multiplier
+            combinedSeries = [x + y for x, y in zip(combinedSeries, multiplied)]
+        newDF.insert(len(newDF.columns), f"value-{i}",combinedSeries,True)
+
+newDF.insert(len(newDF.columns), "is_anomaly",df[df.columns[timeseries+1]],True)
+newDF.to_csv(os.path.join(cwd, "combined.csv"), index=False)
+print("File saved as combined.csv")
